1010a68d67a16e620cff40ec109d474948b67c70
austinelke9407 <austinelke9407@gmail.com>
2026-01-11 21:44:07 +0000

    v6.0.4 "basic db setup"

---
+++ firebase_acc.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+
+    myPost.onSnapshot(doc => {
+        const data = doc.data();
+        document.querySelector('#title').innerHTML = data.title;
+    });
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopUp(provider)
+        .then(result => {
+            const user = result.user;
+            document.write(`Hello ${user.displayName}`);
+            console.log(user);
+        })
+        .catch(console.log);
+}
+
+function updatePost(e) {
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+    myPost.update({ title: e.target.value });
+}
